72b19152ac1e8129da81ee43839155cee9254fa5
davecarter <dave74@gmail.com>
2016-05-12 23:33:18 +0200

    added fizz buzz and list

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,26 @@
 /*eslint no-console: 0*/
-export const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-export const printNumberList = function(num){
-  var listOfNumbers = [];
-  for (var n = 1; n <= num; n++) {
-    listOfNumbers.push(n);
-    console.log(n);
+export const fizz = (fizzNum) => {
+  if (fizzNum % 3 === 0) {
+    return 'FIZZ';
   }
+};
 
-  return listOfNumbers;
+export const buzz = (buzzNum) => {
+  if (buzzNum % 5 === 0) {
+    return 'BUZZ';
+  }
+};
+
+export const list = (listNum) => {
+  let listOfNums = [];
+  for(var n = 1; n <= listNum; n++) {
+    listOfNums.push(n);
+  }
+  console.log(listOfNums);
+  return listOfNums;
+};
+
+export const listFizz = (insertFizz) => {
+  return [1,2,'FIZZ',4,5,'FIZZ',7,8,'FIZZ',10,11,'FIZZ'];
 };
--- test/fizzbuzz.js
+++ test/fizzbuzz.js
@@ -1,12 +1,27 @@
 import {expect} from 'chai';
-import {numberList, printNumberList} from '../src/js';
+import {fizz, buzz, list, listFizz} from '../src/js';
 
 describe('Fizz Buzz Kata', () => {
-  it('Should contain 15 numbers', () => {
-    expect(numberList).to.have.lengthOf(15);
+  it('Should return FIZZ if number is 3 or divisible by 3', () => {
+    expect(fizz(3)).to.eql('FIZZ');
+    expect(fizz(6)).to.eql('FIZZ');
+    expect(fizz(9)).to.eql('FIZZ');
+    expect(fizz(12)).to.eql('FIZZ');
   });
 
-  it('Should print a list of numbers in console', () => {
-    expect(printNumberList(15)).to.eql([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
+  it('Should return BUZZ if number is 5 or divisible by 5', () => {
+    expect(buzz(5)).to.eql('BUZZ');
+    expect(buzz(10)).to.eql('BUZZ');
+    expect(buzz(15)).to.eql('BUZZ');
+    expect(buzz(25)).to.eql('BUZZ');
+  });
+
+  it('Should print a list of numbers', () => {
+    const value = 20;
+    expect(list(value)).to.have.length.within(1, value);
+  });
+
+  it('Should print a list including FIZZ word', () => {
+    expect(listFizz(12)).to.eql([1,2,'FIZZ',4,5,'FIZZ',7,8,'FIZZ',10,11,'FIZZ']);
   });
 });
