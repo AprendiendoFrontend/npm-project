e4f294d5bd990e97fbabc23cfd2659cf40a6255b
David Garcia <david.garcia@scmspain.com>
2016-06-13 23:11:40 +0200

    initial cleanup commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,5 @@
-export const isFizz = (num) => num % 3 === 0;
-export const isBuzz = (num) => num % 5 === 0;
-
-export const fizzbuzz = (num) => {
-  if(isFizz(num) && isBuzz(num)){
-      return 'FIZZBUZZ';
-  }
-
-  if(isFizz(num)){
-    return 'FIZZ';
-  }
-
-  if(isBuzz(num)){
-    return 'BUZZ';
-  }
-
-  return num.toString();
+const changeColor = () => {
+  document.querySelector('.header').addEventListener('click', () => document.body.classList.add('backgnd'));
 };
+
+changeColor();
